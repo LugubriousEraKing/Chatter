630b90965ea1fe8c146d448e31a1e42de332ca85
LugubriousEraKing <149725075+LugubriousEraKing@users.noreply.github.com>
2023-11-09 03:10:49 -0600

    Update Script.js

--- Netlify/Functions/Script.js
+++ Netlify/Functions/Script.js
@@ -1,1 +1,12 @@
+// This is the content of script.js in /netlify/functions/
+exports.handler = async (event, context) => {
+  const userMessage = event.queryStringParameters.message;
+  
+  // Replace this logic with your chatbot's functionality
+  const chatterResponse = `Chatter received: ${userMessage}`;
 
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ response: chatterResponse }),
+  };
+};
